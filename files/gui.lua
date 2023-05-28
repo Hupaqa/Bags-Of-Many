1be36e0ecb52bec391078cf9788f20b8655f030c
Hugo Paquin <paqh@live.ca>
2023-05-28 17:48:57 -0400

    Better spell naming in bags.

--- files/gui.lua
+++ files/gui.lua
@@ -146,7 +146,7 @@ function generate_tooltip(gui, item)
         local item_action_component = EntityGetComponentIncludingDisabled(item, "ItemActionComponent")
         if item_action_component then
             local action_id = ComponentGetValue2(item_action_component[1], "action_id")
-            tooltip = action_id
+            tooltip = "$action_" .. string.lower(action_id)
         end
     -- Potion coloring and tooltip
     elseif EntityHasTag(item, "potion") then
