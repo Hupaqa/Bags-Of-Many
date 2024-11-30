c13f7e5d7d790e022e18c26f8d06ac677a6d50ff
CrazyAmphibian <0crazyamphibian@gmail.com>
2024-11-30 03:17:47 -0800

    code and images

---
+++ files/spells/SPAWN_MEDIUMUNIVERSAL.lua
@@ -0,0 +1,7 @@
+local entity_id = GetUpdatedEntityID()
+if not entity_id  or entity_id==0 then return end
+
+local x, y = EntityGetTransform(entity_id)
+if x and y then
+EntityLoad("mods/bags_of_many/files/entities/bags/bag_universal_medium.xml",x,y)
+end
